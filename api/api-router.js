24649c3f92cbc5f6864cfd976d475d95c509647d
dominique maack <dominiquemaack@gmail.com>
2019-11-11 10:36:38 -0700

    commit

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -9,5 +10,26 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req, res) => {
+  // read a password from the body 
+  // hash the password
+  // restrn it to the user in an object 
+
+  // const credentials = req.body;
+  // const hash = bcrypt.hashSync(credentials.password, 14)
+  // credentials.password = hash;
+
+  // if(!credentials || !bycrypt.compareSync(credentials, credentials.password)) {
+  //   return res.status(401).json({error: 'Incorrect Credentials'})
+  // }
+
+  let password = req.query.password;
+  let hash = bcrypt.hashSync(password, 12)
+  password = hash;
+
+  res.send(hash)
+
+})
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const apiRouter = require('./api-router.js');
-
+const configureMiddleware = require('./configure-middleware');
 const server = express();
+const bcrypt = require('bcryptjs')
 
+
+configureMiddleware(server);
 server.use('/api', apiRouter);
 
 module.exports = server;
